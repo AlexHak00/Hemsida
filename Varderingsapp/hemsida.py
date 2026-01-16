069f99c83d490bb6b1387b6fe2b0ea7e9797f401
AlexHak00 <alex.hakanssonn@gmail.com>
2026-01-16 11:03:59 +0100

    Update hemsida.py

--- Varderingsapp/hemsida.py
+++ Varderingsapp/hemsida.py
@@ -33,12 +33,12 @@ with st.container():
 col1, col2 = st.columns(2)
 
 with col1: 
-    eps = st.number_input("Nuvarande EPS: (exempelvis 5.0)", min_value=1.0, value=1.0, max_value=500, step = 0.1)
+    eps = st.number_input("Nuvarande EPS: (exempelvis 5.0)", min_value=1.0, value=1.0, max_value=500.0, step = 0.1)
 with col2: 
     growth = st.selectbox("Vinsttillväxt ett år framåt (%)", [5, 6 , 8, 10, 12, 15, 18, 20])
 required_return = st.selectbox("Avkastningskrav (%)", [5, 6, 8, 10, 12, 15, 18, 20])
 margin_of_safety = st.selectbox("Säkerhetsmarginal", [0.20, 0.25, 0.30, 0.35, 0.40])
-stockprice = st.number_input("Vad är aktiekursen idag?", min_value=1.0, value=1.0, max_value=5000, step = 0.1)
+stockprice = st.number_input("Vad är aktiekursen idag?", min_value=1.0, value=1.0, max_value=5000.0, step = 0.1)
 
 forward_eps = eps * (1 + growth /100 )
 
@@ -88,6 +88,7 @@ else:
 
 
 ## lägga in så man kan välja ticker 
+
 
 
 
